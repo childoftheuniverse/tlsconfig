cc507305ee1bf2b056bfaf94f412b67091c81e71
Child of the Universe <childoftheuniverse@protonmail.com>
2020-02-02 03:05:26 +0100

    Add another method to load CertPools and use that by default.
    Right now, if a certificate file contains many certificates, only the
    first one is loaded. With this change, we are hoping to fix that.

--- cert_loader.go
+++ cert_loader.go
@@ -24,6 +24,40 @@ func LoadTLSCertificateAndPrivateKey(
 }
 
 /*
+LoadX509Certificates loads a list of X.509 certificates from a PEM encoded
+file.
+*/
+func LoadX509Certificates(certPath string) ([]*x509.Certificate, error) {
+	var pemData []byte
+	var block *pem.Block
+	var remainder []byte
+	var certs []*x509.Certificate = make([]*x509.Certificate, 0)
+	var err error
+
+	if pemData, err = ioutil.ReadFile(certPath); err != nil {
+		return nil, err
+	}
+
+	for len(pemData) > 0 {
+		var cert *x509.Certificate
+		block, remainder = pem.Decode(pemData)
+
+		if len(pemData) == len(remainder) {
+			break
+		}
+
+		if cert, err = x509.ParseCertificate(block.Bytes); err != nil {
+			return certs, err
+		}
+
+		certs = append(certs, cert)
+		pemData = remainder
+	}
+
+	return certs, err
+}
+
+/*
 LoadX509Certificate loads an X.509 certificate from a PEM encoded file.
 */
 func LoadX509Certificate(certPath string) (*x509.Certificate, error) {
@@ -44,16 +78,20 @@ func LoadX509Certificate(certPath string) (*x509.Certificate, error) {
 CertPoolFromFile loads a single X.509 file and creates a CertPool from it.
 */
 func CertPoolFromFile(certPath string) (*x509.CertPool, error) {
+	var certs []*x509.Certificate
 	var cert *x509.Certificate
 	var pool *x509.CertPool
 	var err error
 
-	if cert, err = LoadX509Certificate(certPath); err != nil {
+	if certs, err = LoadX509Certificates(certPath); err != nil {
 		return nil, err
 	}
 
 	pool = x509.NewCertPool()
-	pool.AddCert(cert)
+
+	for _, cert = range certs {
+		pool.AddCert(cert)
+	}
 
 	return pool, nil
 }
