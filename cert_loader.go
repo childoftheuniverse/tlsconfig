8208f0947331bdf3febfae68626d33c811651ffd
Child of the Universe <childoftheuniverse@protonmail.com>
2020-02-06 22:51:00 +0100

    Export start and end times of certificate validities to Prometheus.

--- cert_loader.go
+++ cert_loader.go
@@ -5,7 +5,27 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"io/ioutil"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+var certValidityStartTimes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	Namespace: "tlsconfig",
+	Subsystem: "cert_loader",
+	Name:      "cert_validity_start_times",
+	Help:      "Timestamps in UNIX seconds of when the certificate became valid",
+}, []string{"subject", "serial"})
+var certValidityEndTimes = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	Namespace: "tlsconfig",
+	Subsystem: "cert_loader",
+	Name:      "cert_validity_end_times",
+	Help:      "Timestamps in UNIX seconds of when the certificate expires",
+}, []string{"subject", "serial"})
+
+func init() {
+	prometheus.MustRegister(certValidityStartTimes)
+	prometheus.MustRegister(certValidityEndTimes)
+}
 
 /*
 LoadTLSCertificateAndPrivateKey loads an tls.Certificate from a private and
@@ -13,14 +33,53 @@ public key file in PEM format.
 */
 func LoadTLSCertificateAndPrivateKey(
 	certPath, keyPath string) ([]tls.Certificate, error) {
+	var certs = make([]tls.Certificate, 0)
+	var block *pem.Block
+	var certBytes []byte
+	var keyBytes []byte
 	var cert tls.Certificate
+	var x509cert *x509.Certificate
 	var err error
 
-	if cert, err = tls.LoadX509KeyPair(certPath, keyPath); err != nil {
+	if keyBytes, err = ioutil.ReadFile(keyPath); err != nil {
+		return []tls.Certificate{}, err
+	}
+	if certBytes, err = ioutil.ReadFile(certPath); err != nil {
 		return []tls.Certificate{}, err
 	}
 
-	return []tls.Certificate{cert}, err
+	for len(keyBytes) > 0 && len(certBytes) > 0 {
+		var keyRemainder, certRemainder []byte
+
+		_, keyRemainder = pem.Decode(keyBytes)
+		block, certRemainder = pem.Decode(certBytes)
+
+		if x509cert, err = x509.ParseCertificate(block.Bytes); err != nil {
+			return []tls.Certificate{}, err
+		}
+
+		certValidityStartTimes.With(prometheus.Labels{
+			"subject": x509cert.Subject.String(),
+			"serial":  x509cert.SerialNumber.String(),
+		}).Set(float64(x509cert.NotBefore.Unix()))
+		certValidityEndTimes.With(prometheus.Labels{
+			"subject": x509cert.Subject.String(),
+			"serial":  x509cert.SerialNumber.String(),
+		}).Set(float64(x509cert.NotAfter.Unix()))
+
+		if cert, err = tls.X509KeyPair(certBytes, keyBytes); err != nil {
+			return []tls.Certificate{}, err
+		}
+		if cert.Leaf == nil {
+			cert.Leaf = x509cert
+		}
+
+		keyBytes = keyRemainder
+		certBytes = certRemainder
+		certs = append(certs, cert)
+	}
+
+	return certs, err
 }
 
 /*
@@ -50,6 +109,15 @@ func LoadX509Certificates(certPath string) ([]*x509.Certificate, error) {
 			return certs, err
 		}
 
+		certValidityStartTimes.With(prometheus.Labels{
+			"subject": cert.Subject.String(),
+			"serial":  cert.SerialNumber.String(),
+		}).Set(float64(cert.NotBefore.Unix()))
+		certValidityEndTimes.With(prometheus.Labels{
+			"subject": cert.Subject.String(),
+			"serial":  cert.SerialNumber.String(),
+		}).Set(float64(cert.NotAfter.Unix()))
+
 		certs = append(certs, cert)
 		pemData = remainder
 	}
@@ -61,6 +129,7 @@ func LoadX509Certificates(certPath string) ([]*x509.Certificate, error) {
 LoadX509Certificate loads an X.509 certificate from a PEM encoded file.
 */
 func LoadX509Certificate(certPath string) (*x509.Certificate, error) {
+	var cert *x509.Certificate
 	var pemData []byte
 	var block *pem.Block
 	var err error
@@ -71,7 +140,18 @@ func LoadX509Certificate(certPath string) (*x509.Certificate, error) {
 
 	block, _ = pem.Decode(pemData)
 
-	return x509.ParseCertificate(block.Bytes)
+	if cert, err = x509.ParseCertificate(block.Bytes); err != nil {
+		return nil, err
+	}
+	certValidityStartTimes.With(prometheus.Labels{
+		"subject": cert.Subject.String(),
+		"serial":  cert.SerialNumber.String(),
+	}).Set(float64(cert.NotBefore.Unix()))
+	certValidityEndTimes.With(prometheus.Labels{
+		"subject": cert.Subject.String(),
+		"serial":  cert.SerialNumber.String(),
+	}).Set(float64(cert.NotAfter.Unix()))
+	return cert, nil
 }
 
 /*
